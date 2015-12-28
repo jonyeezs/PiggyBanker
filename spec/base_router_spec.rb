00c88e456d42ae96c517183f88e988af6d83b99f
jonyeezs <jonyeezs@gmail.com>
2015-12-28 16:33:05 +1000

    add title to error page. fix up spec

---
+++ spec/base_router_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'spec_helper'
+require 'tilt/erb'
+require 'json'
+
+include Rack::Test::Methods
+
+def app
+  BaseRouter
+end
+
+describe 'base router' do
+  it 'should handle request with OK response' do
+    get '/'
+    assert_equal last_response.ok?, true
+  end
+
+  it 'should handle request leading to 404' do
+    get '/fakefakefake'
+    assert_equal last_response.status, 404
+    last_response.body.must_include '404 PiggyBanker'
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'sinatra/base'
 require 'rack/test'
+require_relative '../server'
 
 # Load all files to test
 Dir[File.expand_path('..') + '/app/**/*.rb'].each do |file|
