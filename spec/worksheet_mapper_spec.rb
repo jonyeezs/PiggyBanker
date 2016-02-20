f4d3484da29706070d44b5419f586eafe1791278
jonyeezs <jonyeezs@gmail.com>
2016-02-21 01:16:43 +1000

    add unittest for all methods

--- spec/worksheet_mapper_spec.rb
+++ spec/worksheet_mapper_spec.rb
@@ -25,7 +25,7 @@ class MockWorksheet # Implementation simplified from GoogleDrive::Worksheet
   end
 end
 
-describe WorksheetMapper do
+describe 'WorksheetMapper' do
   before do
     @worksheet = MockWorksheet.new
     @num_of_incomes = 1
@@ -43,37 +43,40 @@ describe WorksheetMapper do
     @worksheet[10] =  ['Internet', 'bill', 'monthly', '60.50']
     @worksheet[11] =  ['Movies', 'leisure', 'monthly', '40.70']
     @worksheet.rows_count = 12
+    @subject = SubjectFactory.new.with_module(WorksheetMapper)
   end
 
-  describe 'common library' do
+  describe 'Common' do
+    # TODO: figure out why this isn't working let(:subject) { Class.new { include WorksheetMapper } }
+
     describe 'starting_range' do
       it 'should return the correct length of ranges' do
         test_value = mock(num_rows: 16)
-        WorksheetMapper.starting_range(test_value).size.must_equal 11
+        @subject.starting_range(test_value).size.must_equal 11
       end
     end
     describe 'income_header' do
       it 'should return true for the appropriate string' do
-        WorksheetMapper.income_header?('INCoME').must_equal true
-        WorksheetMapper.income_header?('incoming').must_equal false
+        @subject.income_header?('INCoME').must_equal true
+        @subject.income_header?('incoming').must_equal false
       end
     end
     describe 'expense_header' do
       it 'should return true for the appropriate string' do
-        WorksheetMapper.expense_header?('Expenses').must_equal true
-        WorksheetMapper.expense_header?('INCoME').must_equal false
+        @subject.expense_header?('Expenses').must_equal true
+        @subject.expense_header?('INCoME').must_equal false
       end
     end
     describe 'broken_row' do
       it 'should check if a row in a worksheet has any empty cell' do
-        WorksheetMapper.broken_row?(@worksheet, 7).must_equal true
-        WorksheetMapper.broken_row?(@worksheet, 8).must_equal true
-        WorksheetMapper.broken_row?(@worksheet, 9).must_equal false
+        @subject.broken_row?(@worksheet, 7).must_equal true
+        @subject.broken_row?(@worksheet, 8).must_equal true
+        @subject.broken_row?(@worksheet, 9).must_equal false
       end
     end
     describe 'map_item' do
       it 'should map item with - multiplier for expense' do
-        item = WorksheetMapper.map_item @worksheet, 11, -1
+        item = @subject.map_item @worksheet, 11, -1
         item.id.must_equal 11
         item.description.must_equal @worksheet[11, 1]
         item.category.must_equal @worksheet[11, 2]
@@ -81,19 +84,36 @@ describe WorksheetMapper do
         item.debit?.must_equal true
       end
     end
+  end
 
-    describe 'ExpenseMapper' do
-      it 'should map only expenses' do
-        results = WorksheetMapper.map_expenses @worksheet
-        results.length.must_equal @num_of_expenses
-      end
+  describe 'Expense' do
+    it 'should map only expenses' do
+      results = @subject.map_expenses @worksheet
+      results.length.must_equal @num_of_expenses
+    end
+  end
+
+  describe 'Income' do
+    it 'should map only incomes' do
+      results = @subject.map_incomes @worksheet
+      results.length.must_equal @num_of_incomes
+    end
+  end
+
+  describe 'Article' do
+    it 'should return the year for a budget title' do
+      @subject.year_from_title('2016 Budget').must_equal '2016'
+      @subject.year_from_title('BUDGET 2020').must_equal '2020'
     end
 
-    describe 'IncomeMapper' do
-      it 'should map only incomes' do
-        results = WorksheetMapper.map_incomes @worksheet
-        results.length.must_equal @num_of_incomes
-      end
+    it 'should return the year for an actual title' do
+      @subject.year_from_title('2016 actual').must_equal '2016'
+      @subject.year_from_title('Actual 2020').must_equal '2020'
+    end
+
+    it 'should get all expenses and incomes items' do
+      result = @subject.get_items @worksheet
+      result.length.must_equal @num_of_expenses + @num_of_incomes
     end
   end
 end
