1fcef02a479477dafdf208cddccfe3d940993297
jonyeezs <jonyeezs@gmail.com>
2016-02-21 01:12:33 +1000

    add subject factory. create method to build a subject that includes a module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
+require 'minitest/spec'
 require 'mocha/mini_test'
 require 'sinatra/base'
 require 'rack/test'
@@ -8,6 +9,14 @@ require_relative '../server'
 
 # Load all files to test
 Dir[File.expand_path('..') + '/app/**/*.rb'].each do |file|
-  puts file
   require file
+end
+
+class SubjectFactory
+  def with_module(include_module)
+    container = Class.new do
+      include include_module
+    end
+    container.new
+  end
 end
