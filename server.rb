b77d237b192380d452a050c716a2e8041f235ea9
jonyeezs <jonyeezs@gmail.com>
2016-03-19 09:10:17 +1000

    do load base seperately

--- server.rb
+++ server.rb
@@ -16,6 +16,7 @@ module PiggyBanker
     Server.new do
       load_paths '/app/lib'
       load_paths '/app/models'
+      load_base_route
       load_routes
     end
   end
@@ -26,8 +27,10 @@ module PiggyBanker
     end
 
     def load_routes
-      load_base_route
-      # Load all route files
+      # Refer to: https://www.safaribooksonline.com/library/view/sinatra-up-and/9781449306847/ch04.html
+      # Rack DSL offers a method besides use and run: map.
+      # This nifty method allows you to map a given path to a Rack endpoint.
+      # We can use that to serve multiple Sinatra apps from the same process
       Dir[File.dirname(__FILE__) + '/app/routes/**.rb'].each do |file_path|
         next if file_path.include? 'base_router'
         require file_path
@@ -38,12 +41,8 @@ module PiggyBanker
 
     private
 
-    def load_base_route
+    def load_base_route # TODO: find out if order matters whe requiring
       require_relative PiggyBanker.root + '/app/routes/base_router'
-      # Refer to: https://www.safaribooksonline.com/library/view/sinatra-up-and/9781449306847/ch04.html
-      # Rack DSL offers a method besides use and run: map.
-      # This nifty method allows you to map a given path to a Rack endpoint.
-      # We can use that to serve multiple Sinatra apps from the same process
       map('/') { run BaseRouter }
     end
   end
