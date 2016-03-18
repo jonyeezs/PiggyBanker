2c060fed31673fe29ca485e147d811f95a3ce3cd
jonyeezs <jonyeezs@gmail.com>
2016-03-18 22:54:25 +1000

    rename to plural

--- app/routes/budgets_router.rb
+++ app/routes/budgets_router.rb
@@ -1,14 +1,11 @@
 require 'spreadsheet_adapter'
 # TODO: spec it
 # FIXME: find out why constantize removes the trailing s
-class Budget < BaseRouter
+class Budgets < BaseRouter
   adapter = Spreadsheet::Adapter.new PiggyBanker.settings['spreadsheet_key']
 
-  get '/available' do
-    respond_with availability: adapter.spreadsheet_available?
-  end
-
-  get '/all' do
+  get '' do
+    return response_internal_error unless adapter.spreadsheet_available?
     respond_with budgets: adapter.budgets
   end
 end
