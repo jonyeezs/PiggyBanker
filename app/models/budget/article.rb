904e0138116d3940196f141a9c29de0d9fc61467
jonyeezs <jonyeezs@gmail.com>
2016-05-08 00:55:29 +1000

    make items public

--- app/models/budget/article.rb
+++ app/models/budget/article.rb
@@ -5,7 +5,8 @@ module Model
     class Article
       ERROR_MSG = 'This is not a budget item'
       private_constant :ERROR_MSG
-      attr_reader :items, :year
+      attr_reader :year
+      attr_accessor :items
 
       def initialize(year, items = [])
         @year = year
@@ -23,6 +24,18 @@ module Model
 
       def update_item(updated)
         @items[get_index(updated)] = updated
+      end
+
+      def change_occurances!(occurance_type)
+        @items.each { |item| item.occurance = occurance_type }
+      end
+
+      def debit_items
+        @items.find_all(&:debit?)
+      end
+
+      def credit_items
+        @items.find_all(&:credit?)
       end
 
       private
