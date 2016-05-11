7b60984b7dc2bd1c76bbd52dfb55ffdb8ea94206
jonyeezs <jonyeezs@gmail.com>
2016-05-11 23:55:19 +1000

    initialize budget only once

--- app/routes/budgets_router.rb
+++ app/routes/budgets_router.rb
@@ -2,20 +2,24 @@ require 'lib/adapters/budgets'
 
 class Budgets < BaseRouter
   get '/' do
-    respond_with budgets: Adapter::Budgets.new.articles
+    respond_with budgets: budget.articles
   end
 
   get '/years' do
-    respond_with years: Adapter::Budgets.new.available_years
+    respond_with years: budget.available_years
   end
 
   get '/years/:year' do
-    articles = Adapter::Budgets.new.by_year params[:year]
+    articles = budget.by_year params[:year]
     # queries
     articles.change_occurances! params['occurance'] if params.key? 'occurance'
     articles.items = filter_items_by_transaction(articles, params['transaction_type']) if params.key? 'transaction_type'
 
     respond_with budget: articles.items
+  end
+
+  def budget
+    @budgets ||= Adapter::Budgets.new
   end
 
   def filter_items_by_transaction(articles, transaction)
