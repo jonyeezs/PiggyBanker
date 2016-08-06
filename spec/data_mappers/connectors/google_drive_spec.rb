389c8c0024d5a7b920a26435938cf899b872ae45
jonyeezs <jonyeezs@gmail.com>
2016-08-06 23:14:21 +1000

    add unit test for tables

--- spec/data_mappers/connectors/google_drive_spec.rb
+++ spec/data_mappers/connectors/google_drive_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'lib/data_mappers/connectors/google_drive'
+require 'google_drive'
 
 describe DataMappers::Connectors::GoogleDriveConnector do
   describe 'without key' do
@@ -12,12 +13,21 @@ describe DataMappers::Connectors::GoogleDriveConnector do
   describe 'with key' do
     before do
       @fake_session = mock('object')
+      @fake_spreadsheet = mock('GoogleDrive::Spreadsheet')
       GoogleDrive.stubs(:saved_session).returns(@fake_session)
-      @fake_session.expects(:spreadsheet_by_key)
+      @fake_session.expects(:spreadsheet_by_key).returns(@fake_spreadsheet)
     end
 
     it 'should initialize with a spreadsheet' do
       DataMappers::Connectors::GoogleDriveConnector.new 'somekey'
+    end
+
+    describe 'tables' do
+      it 'should return the available worksheets' do
+        @fake_spreadsheet.expects(:worksheets).once
+        subject = DataMappers::Connectors::GoogleDriveConnector.new 'somekey'
+        subject.tables
+      end
     end
   end
 end
